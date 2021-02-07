720bf9ec04df69985a301d8fcffdf912c3af183c
Alek Tobias <alektobias@gmail.com>
2021-02-07 14:32:53 -0300

    feat: firt pages

--- frontend/src/pages/services/[service]/[status].tsx
+++ frontend/src/pages/services/[service]/[status].tsx
@@ -1,24 +1,25 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 import { Typography, Table } from 'antd';
 import tableColumns from './tableColumns.json';
+import axios from 'axios';
 
 const { Title } = Typography;
-const Service: React.FC = () => {
 
-  const router = useRouter()
-  const { service, status } = router.query
-
+const Service = ({title, data}) => {
   return (
     <>
-      <Title level={2}>
-        {` ${status} ${service}` }
-      </Title>
+      <Title level={2}>{title}</Title>
       <section>
-        <Table columns={tableColumns} />
+        <Table columns={tableColumns} dataSource={data}/>
       </section>
     </>
   );
 }
+Service.getInitialProps = async (ctx) => {
+  const { service, status } = ctx.query
+  
+  const {data} = await axios.get('https://us-central1-teste-869ba.cloudfunctions.net/api/requests');
+  return { title: status + ' ' + service, data }
+} 
 
 export default Service;
