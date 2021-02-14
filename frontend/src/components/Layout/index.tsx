a4c2d46b6fd5308ff14a51dc0b418d67c7db9fbe
Alek Tobias <alektobias@gmail.com>
2021-02-14 11:15:28 -0300

    chore: minor patch on auth hook

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
-import { Layout as LayoutAntd } from 'antd';
+import React, { useEffect } from 'react';
+import { Layout as LayoutANTD} from 'antd';
 import Menu from './Menu';
+import { useAuth } from '../../hooks/auth';
+import { useRouter } from 'next/router';
 
+const { Header, Footer, Sider, Content } = LayoutANTD;
 
-const { Sider, Header, Content, Footer } = LayoutAntd
+const Layout: React.FC = ({children}) => {
+  const { isLogged } = useAuth();
+  const { push, pathname } = useRouter();
 
-const Layout: React.FC = ({ children }) => {
+  // useEffect(() => {
+  //   if(pathname !== '/login' && pathname !== '/register' && !isLogged)  {
+  //     push('/login')
+  //   }
+  // }, [isLogged, pathname])
 
-	// if(false) {
-	// 	return <>{children}</>
-	// }
+  // if(!isLogged) {
+  //   return <>{children}</>
+  // }
+  
+  return (<>{children}</>)
 
-	return (
-		<LayoutAntd>
-			<Sider>
-				<Menu />
-			</Sider>
-			<Layout>
-				<Header>Header</Header>
-				<Content style={{ minHeight: "calc(100vh - 134px)", padding: "24px 48px" }}>
-					{children}
-				</Content>
-				<Footer style={{ textAlign: "center" }}>
-					Copyright © Litesense 2021
-				</Footer>
-			</Layout>
-		</LayoutAntd>
-	)
+  return (
+    <Layout>
+      <Sider>
+        <Menu/>
+      </Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content style={{ minHeight: "calc(100vh - 134px)", padding: "24px 48px" }}>
+          {children}
+        </Content>
+        <Footer style={{textAlign: "center"}}>
+          Copyright © Litesense 2021
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default Layout;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import 'antd/dist/antd.css';
-import Layout from '../components/Layout';
+import { AuthProvider } from '../hooks/auth';
 import GlobalStyle from "../styles/GlobalStyle"
 
 function MyApp({ Component, pageProps }) {
   
   return (
-    <>
+    <AuthProvider>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   )
 }
 
