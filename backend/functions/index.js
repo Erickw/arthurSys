7211a1b957cec54536c3057673ae14fa64d07917
erickw <erickwilly@hotmail.com>
2021-03-28 16:01:55 -0300

    add cors

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,8 +2,11 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const app = require("express")();
+const cors = require("cors");
 const routes = require("./routes");
 
+app.use(cors());
+app.options("*", cors());
 app.use(routes);
 
 exports.api = functions.https.onRequest(app);
