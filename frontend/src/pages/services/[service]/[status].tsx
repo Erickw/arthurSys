b32fe5d75102345d0e15358f32c20296a8e245ee
Talles <talles17.a@hotmail.com>
2021-04-08 17:10:32 -0300

    fix: display only requests made by user logged

--- frontend/src/pages/services/[service]/[status].tsx
+++ frontend/src/pages/services/[service]/[status].tsx
@@ -2,13 +2,13 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from 'react';
-import { Typography, Table, Space, Modal, Descriptions } from 'antd';
+import { Typography, Table, Space, Modal } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 import api from '../../../clients/api';
 
 import { useAuth } from '../../../hooks/auth';
-import { RequestInfo } from '../../../styles/pages/request';
+import DisplayRequestInfo from '../../../components/DisplayRequestInfo';
 
 const { Title } = Typography;
 const { confirm } = Modal;
@@ -18,7 +18,10 @@ interface ServiceProps {
   products?: ProductProps[];
 }
 
-export default function Service({ status }: ServiceProps): JSX.Element {
+export default function Service({
+  status,
+  products,
+}: ServiceProps): JSX.Element {
   const [requests, setRequests] = useState<RequestProps[] | undefined>();
   const { user } = useAuth();
   const [tableIsLoading, setTableIsLoading] = useState(false);
@@ -38,95 +41,6 @@ export default function Service({ status }: ServiceProps): JSX.Element {
         );
       },
     });
-  }
-
-  function handleDisplayRequestInfo(request: RequestProps) {
-    return (
-      <RequestInfo>
-        {/* <Descriptions
-        title="Produto"
-        layout="vertical"
-        bordered
-        column={{ xxl: 12, xl: 8, lg: 5, md: 5, sm: 2, xs: 1 }}
-      >
-        <Descriptions.Item
-          labelStyle={{ width: '130px', padding: '10px' }}
-          label="Nome do produto"
-        >
-          {product.name}
-        </Descriptions.Item>
-        <Descriptions.Item label="Descrição">
-          {product.description}
-        </Descriptions.Item>
-        <Descriptions.Item label="Valor">{product.value}</Descriptions.Item>
-        <Descriptions.Item label="Pagamento">
-          {product.requiredPayment}
-        </Descriptions.Item>
-        <Descriptions.Item label="Nota">{product.notes}</Descriptions.Item>
-        <Descriptions.Item label="Disponibilidade">
-          {product.available ? 'Disponível' : 'Indisponível'}
-        </Descriptions.Item>
-      </Descriptions> */}
-        <Descriptions
-          title="Endereço"
-          layout="vertical"
-          bordered
-          column={{ xxl: 12, xl: 8, lg: 5, md: 5, sm: 2, xs: 1 }}
-        >
-          <Descriptions.Item label="Estado">
-            {request.address.state}
-          </Descriptions.Item>
-          <Descriptions.Item label="Cidade">
-            {request.address.city}
-          </Descriptions.Item>
-          <Descriptions.Item label="CEP">
-            {request.address.postalCode}
-          </Descriptions.Item>
-          <Descriptions.Item label="Bairro">
-            {request.address.district}
-          </Descriptions.Item>
-          <Descriptions.Item label="Rua">
-            {request.address.street}
-          </Descriptions.Item>
-          <Descriptions.Item label="Número">
-            {request.address.number}
-          </Descriptions.Item>
-        </Descriptions>
-
-        <Descriptions
-          title="Requisção"
-          layout="vertical"
-          bordered
-          column={{ xxl: 12, xl: 8, lg: 5, md: 5, sm: 2, xs: 1 }}
-        >
-          <Descriptions.Item label="Nome do paciente">
-            {request.patientName}
-          </Descriptions.Item>
-          <Descriptions.Item label="Email do paciente">
-            {request.patientEmail}
-          </Descriptions.Item>
-          <Descriptions.Item label="Produto">
-            {request.productId}
-          </Descriptions.Item>
-          <Descriptions.Item label="Data">
-            {new Intl.DateTimeFormat('pt-br').format(new Date(request.date))}
-          </Descriptions.Item>
-        </Descriptions>
-
-        <Descriptions
-          title="Informações adicionais do produto"
-          layout="vertical"
-          bordered
-          column={{ xxl: 12, xl: 8, lg: 5, md: 5, sm: 2, xs: 1 }}
-        >
-          {request.fieldsValues.map(fieldValue =>
-            Object.entries(fieldValue.fields).map(item => (
-              <Descriptions.Item label={item[0]}>{item[1]}</Descriptions.Item>
-            )),
-          )}
-        </Descriptions>
-      </RequestInfo>
-    );
   }
 
   useEffect(() => {
@@ -194,7 +108,12 @@ export default function Service({ status }: ServiceProps): JSX.Element {
           loading={tableIsLoading}
           expandable={{
             expandedRowRender: (record: RequestProps) =>
-              handleDisplayRequestInfo(record),
+              DisplayRequestInfo({
+                request: record,
+                product: products.find(
+                  product => product.id === record.productId,
+                ),
+              }),
           }}
         />
       </section>
@@ -202,16 +121,8 @@ export default function Service({ status }: ServiceProps): JSX.Element {
   );
 }
 
-// export const getServerSideProps: GetServerSideProps = async context => {
-//  console.log(context);
-//  console.log('cheogu');
-//  return {
-//    props: { status: 'novo' },
-//  };
-// };
-
 Service.getInitialProps = async ({ query: { status } }) => {
-  // const productsFromApi = await api.get('/products');
-  // const products = productsFromApi.data;
-  return { status };
+  const productsFromApi = await api.get('/products');
+  const products = productsFromApi.data;
+  return { status, products };
 };
