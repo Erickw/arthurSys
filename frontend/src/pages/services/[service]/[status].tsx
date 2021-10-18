808ae4d0dcf69c8f42749b817bd53c05bc0df58d
talles <talles17.a@hotmail.com>
2021-10-17 23:48:31 -0300

    fix: change folder name

--- frontend/src/pages/services/[service]/[status].tsx
+++ frontend/src/pages/services/[service]/[status].tsx
@@ -329,7 +329,7 @@ export const getServerSideProps: GetServerSideProps = async ({
     return {
       props: {
         status,
-        requestsFromApi: requestsWithoutTestRequest,
+        requestsFromApi: requests,
         isAdmin: true,
       },
     };
